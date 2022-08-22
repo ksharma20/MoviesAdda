1c2e6061a9b25cc8543820d884e3506e4f16c0fb
ksharma20 <ksharma0001993@gmail.com>
2022-08-22 22:55:13 +0530

    Added Allowed Host & StaticFiles

--- favMovies/settings.py
+++ favMovies/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u#nud(jbrkr-jthfpd_k)kto+(h-@&!2+j2!$351=n8q#q!ut4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [ BASE_DIR/'static' ]
+# STATIC_ROOT = BASE_DIR/'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
