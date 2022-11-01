5a2115192d88678c9809da87210e3532d182ffdc
ksharma20 <ksharma0001993@gmail.com>
2022-11-01 15:38:16 +0530

    fix: Jenkinsfile [stages] & setup.sh [python venv]

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 sudo apt update
 sudo apt install python-is-python3
+sudo apt install python-venv
 python -m venv venv
 source venv/bin/activate || source venv/Scripts/activate
 pip install -r requirements.txt
